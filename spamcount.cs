8bb28e6fedb700113f6f7e34cace7764134f6245
Dominik Schratl <hi@dominik-schratl.at>
2022-05-27 11:19:59 +0200

    Changed exercise spamcount to align with new task

--- spamcount.cs
+++ spamcount.cs
@@ -1,15 +1,9 @@
 // Your code here
 
 string[] subjectsAlex = { "Inquiry", "MINE BITCOIN FAST", "Offer", "Re: How much is a website", "Lifehack: Get rich soon", "Send bitcoin" };
-string[] subjectsTom = { "Re: Help with current inquiry", "Bitcoin - Fast and Easy", "Need Offer", "What do we do for lunch", "Lifehack: Get rich soon", "Send bitcoin" };
+string[] subjectsTom = { "Offer: New Macbooks", "Re: Help with current inquiry", "Bitcoin - Fast and Easy", "Need Offer", "What do we do for lunch", "Lifehack: Get rich soon", "Send bitcoin" };
 string[] subjectsAnna = { "Inquiry", "Offer", "Re: How much is an app", "Thanks for attending the meeting" };
 
-int spamCountAlex = 0;
-int spamCountTom = 0;
-int spamCountAnna = 0;
-
-// Your code here
-
-Console.WriteLine($"Alex: {spamCountAlex} messages in your inbox are spam");
-Console.WriteLine($"Alex: {spamCountTom} messages in your inbox are spam");
-Console.WriteLine($"Alex: {spamCountAnna} messages in your inbox are spam");
+int spamCountAlex = 0; // Your code here
+int spamCountTom = 0; // Your code here
+int spamCountAnna = 0; // Your code here
