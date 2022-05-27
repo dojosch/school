ba706b70047f4306888cb1d67a6af63b7618a05b
Dominik Schratl <hi@dominik-schratl.at>
2022-05-27 10:52:44 +0200

    Exercise Spamcount

---
+++ spamcount.cs
@@ -0,0 +1,8 @@
+// Your code here
+
+string[] subjects = { "Inquiry", "Offer", "Re: How much is a website", "Lifehack: Get rich soon", "Send bitcoin" };
+int spamCount = 0;
+
+// Your code here
+
+Console.WriteLine($"{spamCount} messages in your inbox are spam");
