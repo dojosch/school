829617676e066491f20f3ef44e34904623b2d380
Dominik Schratl <hi@dominik-schratl.at>
2022-05-27 11:04:02 +0200

    Added Employees

--- spamcount.cs
+++ spamcount.cs
@@ -1,8 +1,15 @@
 // Your code here
 
-string[] subjects = { "Inquiry", "Offer", "Re: How much is a website", "Lifehack: Get rich soon", "Send bitcoin" };
-int spamCount = 0;
+string[] subjectsAlex = { "Inquiry", "MINE BITCOIN FAST", "Offer", "Re: How much is a website", "Lifehack: Get rich soon", "Send bitcoin" };
+string[] subjectsTom = { "Re: Help with current inquiry", "Bitcoin - Fast and Easy", "Need Offer", "What do we do for lunch", "Lifehack: Get rich soon", "Send bitcoin" };
+string[] subjectsAnna = { "Inquiry", "Offer", "Re: How much is an app", "Thanks for attending the meeting" };
+
+int spamCountAlex = 0;
+int spamCountTom = 0;
+int spamCountAnna = 0;
 
 // Your code here
 
-Console.WriteLine($"{spamCount} messages in your inbox are spam");
+Console.WriteLine($"Alex: {spamCountAlex} messages in your inbox are spam");
+Console.WriteLine($"Alex: {spamCountTom} messages in your inbox are spam");
+Console.WriteLine($"Alex: {spamCountAnna} messages in your inbox are spam");
